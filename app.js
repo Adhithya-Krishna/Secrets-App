93a71e0feb158021c7b8b5f44aff2720791090e2
Adhithya Krishna S <adithyakrshn4@gmail.com>
2023-06-05 12:38:17 +0530

    Updated Security - Level 5 Passport Authentication and Cookies

--- app.js
+++ app.js
@@ -1,87 +1,108 @@
 
-require('dotenv').config()
-const express = require('express');
-const ejs = require('ejs');
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+    require('dotenv').config()
+    const express = require('express');
+    const ejs = require('ejs');
+    const mongoose = require('mongoose');
+    const session = require('express-session');
+    const passport = require('passport');
+    const passportLocalMongoose = require('passport-local-mongoose');
 
 
-const app = express();
+    const app = express();
 
-app.use(express.static('public'));
-app.set('view engine','ejs');
-app.use(express.urlencoded({extended:true}));
+    app.use(express.static('public'));
+    app.set('view engine','ejs');
+    app.use(express.urlencoded({extended:true}));
 
+    // Initialize express-session middleware
 
-main().catch(console.error);
+    app.use(session({
+        secret:"Our little secret.",
+        resave:false,
+        saveUninitialized:true
+    }))
 
-async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/userDB');
-    
-    const userSchema = new mongoose.Schema({
-        email:String,
-        password:String
-    });
+    // Initialize Passport.js middleware
+    app.use(passport.initialize());
 
-    const User = mongoose.model('User',userSchema);
+    //passpot.session middleware if you want to support persistent login sessions
+    app.use(passport.session());
 
-    app.get("/",(req,res)=>{
-        res.render('home');
-    });
-    
-    app.get("/register",(req,res)=>{
-        res.render('register');
-    });
+    main().catch(console.error);
 
-    app.post("/register",async(req,res)=>{
-        try {
-            const hash = await bcrypt.hash(req.body.password, saltRounds);
-            const newUser = new User({
-                email:req.body.username,
-                password:hash
-            });
-            const result = await newUser.save();
-            if(result){
-                res.render('secrets');
+    async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/userDB');
+        
+        const userSchema = new mongoose.Schema({
+            email:String,
+            password:String
+        });
+
+        // Add the Passport-Local-Mongoose plugin to your user schema
+        userSchema.plugin(passportLocalMongoose);
+
+        const User = mongoose.model('User',userSchema);
+
+        passport.use(User.createStrategy());
+
+        passport.serializeUser(User.serializeUser());
+        passport.deserializeUser(User.deserializeUser());
+
+        app.get("/",(req,res)=>{
+            res.render('home');
+        });
+        
+        app.get("/register",(req,res)=>{
+            res.render('register');
+        });
+
+        app.get("/secrets",(req,res)=>{
+            if(req.isAuthenticated()){
+                res.render("secrets")
             }else{
-                console.log("Login Failed");
+                res.redirect("/login");
             }
-        } catch (err) {
-            console.log(err);
-        }
-    });
+        });
 
-
-    app.get("/login",(req,res)=>{
-        res.render('login');
-    });
-    
-    app.post("/login",async(req,res)=>{
-        const username = req.body.username;
-        const password = req.body.password;
-
-        try {
-            const foundUser = await User.findOne({email:username})
-            if(foundUser){
-                const result = await bcrypt.compare(password,foundUser.password);
-                if(result){
-                    res.render('secrets')
+        app.get("/logout",(req,res)=>{
+            req.logOut((err)=>{
+                if(err){
+                    console.log(err);
                 }else{
-                    console.log("Password does not Match!")
+                    res.redirect("/");
                 }
-            }else{
-                console.log("User Not found...")
-            }
-        } catch (err) {
-            console.log(err);
-        }
-    });
-    
-    app.listen(3000,()=>{
-        console.log("Server is runing on port 3000...   ");
-    });
+            });
+            
+        });
+
+        app.post("/register",async(req,res)=>{
+            const username = req.body.username;
+            const password =req.body.password;
+            User.register({username:username},password).then(()=>{
+            const authenticate = passport.authenticate("local");
+            authenticate(req,res,()=>{
+                res.redirect("/secrets")
+            });
+            }).catch(err=>{
+                console.log(err);
+                res.redirect("/register");
+            });
+        });
 
 
-}
+        app.get("/login",(req,res)=>{
+            res.render('login');
+        });
+        
+        app.post("/login", passport.authenticate("local"), (req, res) => {
+            res.redirect("/secrets");
+          });
+          ;
+        
+        app.listen(3000,()=>{
+            console.log("Server is runing on port 3000...   ");
+        });
+
+
+    }
 
